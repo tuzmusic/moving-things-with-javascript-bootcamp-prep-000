2d0d923aff2bb6d2baf435b32a958ddba547a7f5
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-04 23:56:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function moveDodgerLeft() {
 function moveDodgerRight() {
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
-  if (left <= ) { return }
+  if (left <= gameDiv.style.width - dodger.style.width) { return }
   dodger.style.left = `${left + 10}px`
 }
 
