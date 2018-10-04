216114e20625ef4d80a53df96819092d027870d9
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-04 23:19:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-const app = "I don't do much."
+var dodger = $('#dodger')
+
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
+  dodger.style.left = `${left - 10}px`
+}
+
+function moveDodgerRight() {
+
+}
+
+document.addEventListener('keydown', (key) => {
+  console.log("key!");
+  if (key.which === 37) {
+    moveDodgerLeft)()
+  } else if (key.which === 39) {
+    moveDodgerRight()
+  }
+})
