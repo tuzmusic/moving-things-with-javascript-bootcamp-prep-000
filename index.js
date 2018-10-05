a8779bc287a9be1b96c032f0a3bc93743daa2fd8
Jonathan Tuzman <tuzmusic@gmail.com>
2018-10-05 00:29:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function moveDodgerRight() {
 }
 
 document.addEventListener('keydown', function(key) {
+  console.log(key.which);
   if (key.which === 37) {
     moveDodgerLeft()
   } else if (key.which === 39) {
-    debugger
     moveDodgerRight()
   }
 })
